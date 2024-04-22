2b51d0f864607a8ba7965584290df48b28161eda
taxvui <127817880+taxvui@users.noreply.github.com>
2024-04-22 16:57:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
 module.exports = {
   
 
-    MONGODB_USERNAME:"sales",
-
-    MONGODB_PASSWORD:"Fuck1984@!.",
-
-    MONGODB_DB_NAME:"love",
-
 
   //port
   PORT: process.env.PORT || 5000,
@@ -19,17 +13,17 @@ module.exports = {
   MONGODB_DB_NAME:"love",
 
   //secret key for API
-  SECRET_KEY: "SECRET_KEY",
+  SECRET_KEY: "ssKstE5NSY",
 
   //gmail credentials for send email
   EMAIL: "EMAIL",
   PASSWORD: "PASSWORD",
 
   //secret key for jwt
-  JWT_SECRET: "JWT_SECRET",
+  JWT_SECRET: "ssKstE5NSY",
 
   //baseUrl
-  baseURL: "https://yourDomain.com/",
+  baseURL: "https://lovebabe.netlify.app/",
 
   //firebase server key for notification
   SERVER_KEY: "AAAAoV65bA0:APA91bGTU0yChaV8ENQRv-9C5AJuhOJwq9QDJKmLZjN80QwKd7gqCcvM5pGPkfKiuAf5TWX2IyPQvZHAlnHcFta-qQQfQcT5GDlfKka2dG7zrX11aX-zqtc8qb6DeU2P_m1pK3YEkZ2_",
